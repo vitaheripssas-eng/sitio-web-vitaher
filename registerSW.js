55f89672c9407e6b380ccbda3ff80b7a0720b2d9
SALUDOMI <saludomigeneral01@gmail.com>
2026-09-10 23:11:24 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sitio-web-vitaher/sw.js', { scope: '/sitio-web-vitaher/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
