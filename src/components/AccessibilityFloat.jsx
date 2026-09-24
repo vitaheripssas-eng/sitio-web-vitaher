995a0984834a31efa5ed7e0df895b85ccd6bfe72
SALUDOMI <saludomigeneral01@gmail.com>
2026-09-24 16:54:51 -0500

    style: FA universal-access 28->52 acoplado al fondo azul 52

--- src/components/AccessibilityFloat.jsx
+++ src/components/AccessibilityFloat.jsx
@@ -143,7 +143,7 @@ export default function AccessibilityFloat() {
         onPointerUp={handlePointerUp}
         style={btnStyle}
       >
-        <FontAwesomeIcon icon={faUniversalAccess} style={{ fontSize: '28px', width: '28px', height: '28px' }} />
+        <FontAwesomeIcon icon={faUniversalAccess} style={{ fontSize: '52px', width: '52px', height: '52px' }} />
       </button>
 
       {open && (
