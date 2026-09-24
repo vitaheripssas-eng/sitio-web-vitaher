a885258d856910e4026851c664ccc640e05990ea
SALUDOMI <saludomigeneral01@gmail.com>
2026-09-24 16:07:42 -0500

    style: icono accesibilidad 24->34 para llenar boton 56px

--- src/components/AccessibilityFloat.jsx
+++ src/components/AccessibilityFloat.jsx
@@ -141,7 +141,7 @@ export default function AccessibilityFloat() {
         onPointerUp={handlePointerUp}
         style={btnStyle}
       >
-        <Icon name="accessibility" size={24} />
+        <Icon name="accessibility" size={34} />
       </button>
 
       {open && (
