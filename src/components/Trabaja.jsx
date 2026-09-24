0274b6cc9d315843e74b444801820dac128b3481
SALUDOMI <saludomigeneral01@gmail.com>
2026-09-24 14:52:07 -0500

    feat: trabaja 4 cards exacto imagen 2x2 icono documento lila

--- src/components/Trabaja.jsx
+++ src/components/Trabaja.jsx
@@ -15,22 +15,22 @@ const FORMATOS = [
   {
     label: 'Cuidador',
     href: `${BASE}formatos/FORMATO_LISTA_DE_CHEQUEO_CUIDADOR.pdf`,
-    icon: 'user-check',
+    icon: 'file',
   },
   {
     label: 'Profesional',
     href: `${BASE}formatos/FORMATO_LISTA_DE_CHEQUEO_PROFESIONAL.pdf`,
-    icon: 'brain',
+    icon: 'file',
   },
   {
     label: 'Aux. Enfermería',
     href: `${BASE}formatos/FORMATO_LISTA_DE_CHEQUEO_AUX_ENFERMERIA.pdf`,
-    icon: 'user',
+    icon: 'file',
   },
   {
     label: 'Administrativo',
     href: `${BASE}formatos/FORMATO_LISTA_DE_CHEQUO_ADMINISTRATIVO.pdf`,
-    icon: 'home',
+    icon: 'file',
   },
 ]
 
@@ -132,12 +132,9 @@ export default function Trabaja() {
                   key={formato.label}
                 >
                   <span className="formato-icon">
-                    <Icon name={formato.icon} size={28} />
+                    <Icon name={formato.icon} size={24} />
                   </span>
                   <h4>{formato.label}</h4>
-                  <small>
-                    <Icon name="download" size={14} /> Descargar lista
-                  </small>
                 </a>
               ))}
             </div>
